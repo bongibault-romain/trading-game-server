30aedd460b69a332c45d3705c591ee10cea42667
Bongibault Romain <bongibault.romain@gmail.com>
2025-09-20 11:15:59 +0200

    fix: update default server port from 3000 to 3333

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import server from "./server";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3333;
+
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
